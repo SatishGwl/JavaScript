00645512c8b63ba5d3d4297b972121263d839c45
Satish Sharma <satish.srcem@gmail.com>
2023-09-12 08:11:17 +0000

    variable declaration with var and issue in var

---
+++ .devcontainer/01_Basics/variables.js
@@ -0,0 +1,12 @@
+let accountEmail = "satishgmail.com"
+// const accountNumber = '1234'
+var Id = '4444'
+city = 'pune'
+// prefer not to use var because of its block and function scope.
+accountEmail = 'google.com',
+accountNumber = '789',
+city = 'gurugram'
+Id = '000'
+
+
+console.table([accountEmail, accountNumber, city, Id ])
